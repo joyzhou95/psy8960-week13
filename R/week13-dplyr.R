06113311ca3c0e8e867d3e10d7ff8c7ecdf0f5be
Joy Zhou <zhou1559@umn.edu>
2023-04-28 10:24:18 -0500

    Commented codes

--- R/week13-dplyr.R
+++ R/week13-dplyr.R
@@ -5,6 +5,8 @@ library(keyring)
 library(RMariaDB)
 
 # Data Import and Cleaning
+## Log into UMN LATIS server using my UMN credentials and the certificate downloaded from their website,
+## already set up the password using the keyring package
 conn <- dbConnect(MariaDB(),
                   user = "zhou1559",
                   password = key_get("latis_sql","zhou1559"),
@@ -12,27 +14,40 @@ conn <- dbConnect(MariaDB(),
                   port = 3306,
                   ssl.ca = '../mysql_hotel_umn_20220728_interm.cer')
 
+## Send a comment to access the cla_tntlab folder 
 dbExecute(conn, "USE cla_tntlab;")
 
+## Select everything in the datascience_8960_table and save it as week13_tbl
 week13_tbl <- dbGetQuery(conn, "SELECT *
                          FROM datascience_8960_table;")
 
+## Save week13_tbl as a csv file in the data folder
 write_csv(week13_tbl, "../data/week13.csv") 
 
 # Analysis
+
+## Calculate the number of managers by counting the number of rows in the table
 nrow(week13_tbl)
 
+## Count the number of unique managers by counting the number of unique employee ids in the table 
 n_distinct(week13_tbl$employee_id)
 
+## Count the number of managers in each city by first filtering the ones that were not hired as managers initally,
+## then counting the frequency of each city appearing in the table 
 week13_tbl %>%
   filter(manager_hire == "N") %>%
   count(city)
-  
+
+##First group the dataset by the performance levels, then create now columns that contain the mean and sd
+## of years employed in each group 
 week13_tbl %>%
   group_by(performance_group) %>%
   summarize(mean_employ = mean(yrs_employed),
             sd_employ = sd(yrs_employed))
 
+## First sort the table by city and then by test scores (descending order), then group by city and select 
+## the top three managers based on their test scores using slice_max, and finally only display these managers'
+## employee_id and their location
 week13_tbl %>%
   arrange(city, desc(test_score)) %>%
   group_by(city) %>%
--- R/week13-sql.R
+++ R/week13-sql.R
@@ -1,8 +1,11 @@
+# Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(keyring)
 library(RMariaDB)
 
 # Data Import and Cleaning
+## Log into UMN LATIS server using my UMN credentials and the certificate downloaded from their website,
+## already set up the password using the keyring package
 conn <- dbConnect(MariaDB(),
                   user = "zhou1559",
                   password = key_get("latis_sql","zhou1559"),
@@ -10,16 +13,23 @@ conn <- dbConnect(MariaDB(),
                   port = 3306,
                   ssl.ca = '../mysql_hotel_umn_20220728_interm.cer')
 
+## Send a comment to access the cla_tntlab folder 
 dbExecute(conn, "USE cla_tntlab;")
 
+## From the datascience_8960_table, select the manager_hire column, count its row number and save it as manager_count, 
+## which represents the number of managers in the dataset 
 dbGetQuery(conn, "SELECT 
            COUNT(manager_hire) AS manager_count
            FROM datascience_8960_table;")
 
+## From the datascience_8960_table, select the employee_id column, count only the unique ids and save it as manager_unique_count, 
+## which represents the number of unique managers in the dataset  
 dbGetQuery(conn, "SELECT 
            COUNT(DISTINCT employee_id) AS manager_unique_count
            FROM datascience_8960_table;")
 
+## From the datascience_8960_table, filter for the employees that were not hired as managers upon entering the company, 
+## then group the table by city, lastly select the city column, count its row numbers and save it as manager_city_count, 
 dbGetQuery(conn, "SELECT 
            city, 
            COUNT(city) AS manager_city_count 
@@ -27,6 +37,9 @@ dbGetQuery(conn, "SELECT
            WHERE manager_hire = 'N'
            GROUP BY city;")
 
+## From the datascience_8960_table, first group by performance levels, then select the columns of performance levels,
+## and calculate the mean and sd of years employed as mean_employ and sd_employ so that the final display includes the mean,
+## sd and also their corresponding performance levels 
 dbGetQuery(conn, "SELECT 
            performance_group, 
            AVG(yrs_employed) AS mean_employ, 
@@ -34,6 +47,9 @@ dbGetQuery(conn, "SELECT
            FROM datascience_8960_table
            GROUP BY performance_group;")
 
+## From the datascience_8960_table, first partition the dataset by city and sort it by the descending order of test scores,
+## then create a column called rank_num, which represents the ranking of managers by test scores within each city, and save the new table as psy8960_table_rank_num 
+## From the new table, select the employee_id and location of those ranked within top 3 
 dbGetQuery(conn, "WITH psy8960_table_rank_num AS (
            SELECT *,
            RANK() OVER(PARTITION BY city ORDER BY test_score DESC) AS rank_num
